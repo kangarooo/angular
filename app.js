3d1f1029f65a56c08e644b34ee8f5adb9a6a6a1c
Kangarooo <kangarooo@gmail.com>
2016-03-07 13:37:59 +0200

    not working

--- app.js
+++ app.js
@@ -2,27 +2,40 @@ var app = angular.module('firstApp', []);
 
 var _statuses = [];
 
-  app.controller('UserController', function($rootScope){
+  app.controller('UserController', function(){
+
+
     var vm = this;
 
+__resetForm();
 
-    vm.user = '';
-    vm.messege = '';
+
+vm.setStatus = _setStatus;
 
     vm.setStatus = function() {
-      var _userToSend = {
+      var _newStatus = {
         user: vm.user,
         messege: vm.messege
+
       };
 
-        console.log('Sending user status');
+        console.log('Sending user status:');
         console.log(JSON.stringify(_userToSend));
 
 
-        _statuses.push(_userToSend);
-    }
-});
+        _statuses.push(_newStatus);
 
+        __resetForm();
+    }
+
+
+function __resetForm() {
+
+      vm.user = '';
+      vm.messege = '';
+
+}
+});
 
 app.controller('StatusController', function ($rootScope){
   var vm = this;

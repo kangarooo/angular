1fb47458bb3483a126134063bc78a8652921c22f
Piotr Janus <p.janus@accenture.com>
2016-03-09 11:11:19 +0200

    server side data validation

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -6,10 +8,12 @@ var cors = require('cors');
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var _statuses = [];
+
+var MESSAGE_LIMIT = 100;
 
 app.use(express.static('app'));
 
@@ -18,14 +22,47 @@ app.get('/statuses', function (req, res) {
 });
 
 app.post('/statuses', function (req, res) {
-    var newStatus = req.body;
-    newStatus.id = _statuses.length;
+    var newStatus = {
+        id: _statuses.length,
+        user: _.toString(_.get(req.body, 'user')).trim(),
+        message: _parseMessage(_.get(req.body, 'message')),
+        date: new Date(),
+        sendDate: new Date(_.get(req.body, 'date'))
+    };
 
-    console.log('Adding new status:');
-    console.log(newStatus);
+    if (_validateStatus(newStatus)) {
+        console.log('Adding new status:');
+        console.log(newStatus);
+        _statuses.push(newStatus);
+        res.status(201).json(newStatus);
+    } else {
+        console.error('Received invalid message:');
+        console.error(newStatus);
+        res.status(404).send('Message was not valid.');
+    }
 
-    _statuses.push(newStatus);
-    res.status(201).json(newStatus);
+    function _parseMessage(message) {
+        var newMessage = _.toString(message);
+        var TRUNC = '...';
+
+        if (newMessage.length > MESSAGE_LIMIT) {
+            return newMessage.substr(0, MESSAGE_LIMIT - TRUNC.length) + TRUNC;
+        } else {
+            return newMessage;
+        }
+    }
+
+    function _validateStatus(status) {
+        var isValid = true;
+
+        isValid = isValid && !_.isEmpty(status.user);
+
+        isValid = isValid && !_.isEmpty(status.message);
+
+        isValid = isValid && !_.isNaN(status.sendDate.getTime());
+
+        return isValid;
+    }
 });
 
 app.listen(8080, function () {
